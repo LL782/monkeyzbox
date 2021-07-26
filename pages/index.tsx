0cb113c4c22cdb155de5fb278e3241b985d23658
ll782 <laurence.lord@gmail.com>
2021-07-26 17:29:32 +0100

    Red style

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,16 @@ import Layout from "../components/Layout";
 const IndexPage = () => (
   <Layout title="Monkeyzbox - Positive Vibrations">
     <div className="posterText">
-      <h1>Monkeyzbox</h1>
-      <p>You have to go away to come back...</p>
+      <h1 className="posterText__heading">Monkeyzbox</h1>
+      <p className="posterText__body">You have to go away to come back...</p>
     </div>
     <div className="poster">
       <Image
         src={everythingPassesPoster}
         alt="Artwork background with colourful flowers, hearts and faces on a black background"
         layout="fixed"
-        sizes="1346"
+        width={673}
+        height={1024.5}
       />
     </div>
   </Layout>
